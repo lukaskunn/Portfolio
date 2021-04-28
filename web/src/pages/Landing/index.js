69309c420d3b8e9064d58c68f9632af151734ce3
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-04-28 00:57:52 -0300

    finish home page

--- web/src/pages/Landing/index.js
+++ web/src/pages/Landing/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import PageHeader from '../../components/PageHeader'
+import React from "react";
+import PageHeader from "../../components/PageHeader";
+import { FiChevronDown } from "react-icons/fi";
 
-import './styles.css';
+import "./styles.css";
 
 function Landing() {
   return (
-      <div className="landing">
-          <PageHeader />
-          <h1>Landing</h1>
+    <div className="landing">
+      <PageHeader />
+      <div className="content">
+        <div className="home">
+          <div className="title">
+            <h1 className="title-name">Lucas Oliveira</h1>
+            <h4 className="sub-title">Computer Science Student</h4>
+            <h4 className="sub-title">technology enthusiast</h4>
+          </div>
+          <FiChevronDown />
+        </div>
+
+        <div className="about">
+          <h2>About</h2>
+          <p></p>
+        </div>
+        <div className="about">
+          <h2>About</h2>
+          <p></p>
+        </div>
+        <div className="about">
+          <h2>About</h2>
+          <p></p>
+        </div>
+        <div className="about">
+          <h2>About</h2>
+          <p></p>
+        </div>
+        <div className="about">
+          <h2>About</h2>
+          <p></p>
+        </div>
       </div>
+    </div>
   );
 }
 
-export default Landing
+export default Landing;
