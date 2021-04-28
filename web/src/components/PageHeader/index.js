b1daadec36e87ea669ff42c2c27cd6eeba48057c
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-04-28 00:57:17 -0300

    implement static navbar

--- web/src/components/PageHeader/index.js
+++ web/src/components/PageHeader/index.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components'
 
 import './styles.css';
 
-const StyledLink = styled(Link)`
-    text-decoration: none;
-    color: white;
-
-    &:focus, &:hover, &:visited, &:link, &:active {
-        text-decoration: none;
-    }
-`;
-
 function PageHeader() {
+    const [scrolled, setScrolled] = useState(false);
+
+    const changeBackground = () => {
+        if(window.scrollY >= 200){
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    }
+    
+    useEffect(() => {
+        window.addEventListener('scroll', changeBackground);
+    })
+    
+    let x=['page-header'];
+    if(scrolled){
+        x.push('scrolled');
+    }
+
     return (
-        <div className="page-header">
+        <div className={x.join(" ")}>
             <div className="logo">
                 <h3>Lucas OLIVEIRA</h3>
             </div>
             <div className="menu">
                 <Link to="/" className="menu-item">Home</Link>
-                <Link className="menu-item">Sobre</Link>
-                <Link className="menu-item">Home</Link>
-                <Link className="menu-item">Contatos</Link>
+                <Link className="menu-item">About</Link>
+                <Link className="menu-item">Projects</Link>
+                <Link className="menu-item">Contacts</Link>
             </div>
         </div>
     );
