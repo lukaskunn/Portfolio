7e04586332d49e14b072806fc961027c66dc18d0
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-04-28 00:56:21 -0300

    add lock scroll to X axis

--- web/src/styles/globalstyles.js
+++ web/src/styles/globalstyles.js
@@ -2,7 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     @import url(https://fonts.googleapis.com/css2?family=Crimson+Text:wght@100;200;300;400;500;600;700&display=swap);
-
+  html{
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+  
   body {
     width: 100vw;
     margin: 0;
