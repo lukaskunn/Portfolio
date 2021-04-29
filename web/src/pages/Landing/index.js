fe8bb4e68a758e5335dc28013a9c0acf7180896c
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-04-29 00:30:44 -0300

    start edit about and project sections

--- web/src/pages/Landing/index.js
+++ web/src/pages/Landing/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
+import ProjectItem from '../../components/ProjectItem'
 import { FiChevronDown } from "react-icons/fi";
 
 import "./styles.css";
+import { IconContext } from "react-icons/lib";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
@@ -14,29 +17,43 @@ function Landing() {
             <h1 className="title-name">Lucas Oliveira</h1>
             <h4 className="sub-title">Computer Science Student</h4>
             <h4 className="sub-title">technology enthusiast</h4>
+            <IconContext.Provider value={{ size: "2.5em" }}>
+              <Link
+                to={{
+                  pathname: "/",
+                  search: "?sort=name",
+                  hash: "#about",
+                  state: { fromDashboard: true },
+                }}
+                className="page-link"
+              >
+                <FiChevronDown className="down-icon" />
+              </Link>
+            </IconContext.Provider>
           </div>
-          <FiChevronDown />
         </div>
 
-        <div className="about">
-          <h2>About</h2>
-          <p></p>
+        <div className="about" id="about">
+          <h2>ABOUT</h2>
+          <div className="about-text">
+            <h3 className="name">Lucas Oliveira</h3>
+            <i>
+              <p>
+                Computer Science student (05/08) and technology enthusiast.
+                Working in 2 web/mobile projects and 1 arduino project. I
+                started programming at the age of 16 with small games. lover of
+                JavaScript and React, currently looking for jobs for web
+                development aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              </p>
+            </i>
+          </div>
         </div>
-        <div className="about">
-          <h2>About</h2>
-          <p></p>
-        </div>
-        <div className="about">
-          <h2>About</h2>
-          <p></p>
-        </div>
-        <div className="about">
-          <h2>About</h2>
-          <p></p>
-        </div>
-        <div className="about">
-          <h2>About</h2>
-          <p></p>
+
+        <div className="projects">
+          <h2>PROJECTS</h2>
+          <div className="project-itens">
+            <ProjectItem title="Today in History"/>
+          </div>
         </div>
       </div>
     </div>
