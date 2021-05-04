f8c58c1d8796412ca6d3f573659183953da3b1c6
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-05-04 01:20:52 -0300

    Create services sections

--- web/src/pages/Landing/index.js
+++ web/src/pages/Landing/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
 import ProjectItem from '../../components/ProjectItem'
+import ServiceItem from '../../components/ServiceItem'
 import { FiChevronDown } from "react-icons/fi";
 
 import "./styles.css";
@@ -38,21 +39,41 @@ function Landing() {
           <div className="about-text">
             <h3 className="name">Lucas Oliveira</h3>
             <i>
+      
               <p>
                 Computer Science student (05/08) and technology enthusiast.
                 Working in 2 web/mobile projects and 1 arduino project. I
-                started programming at the age of 16 with small games. lover of
-                JavaScript and React, currently looking for jobs for web
-                development aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                started programming when i was 16 years old making small games for fun. lover of
+                JavaScript and React, actually working with IT support but looking for jobs for web
+                development or programming. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              </p>
+              <p>
+                Computer Science student (05/08) and technology enthusiast.
+                Working in 2 web/mobile projects and 1 arduino project. I
+                started programming when i was 16 years old making small games for fun. lover of
+                JavaScript and React, actually working with IT support but looking for jobs for web
+                development or programming. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              </p>
+              <p>
+                Computer Science student (05/08) and technology enthusiast.
+                Working in 2 web/mobile projects and 1 arduino project. I
+                started programming when i was 16 years old making small games for fun. lover of
+                JavaScript and React, actually working with IT support but looking for jobs for web
+                development or programming. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
               </p>
             </i>
           </div>
         </div>
-
+        <div className="services">
+          <h2>SERVICES</h2>
+          <div className="services-itens" >
+            <ServiceItem icon="phone"/>
+          </div>
+        </div>
         <div className="projects">
           <h2>PROJECTS</h2>
           <div className="project-itens">
-            <ProjectItem title="Today in History"/>
+            <ProjectItem title="Today in History" background="todayInHistory"/>
           </div>
         </div>
       </div>
