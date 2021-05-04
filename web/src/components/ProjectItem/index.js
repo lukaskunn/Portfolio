5cfa188a07bc3d2ff06f58406f1641669af1b4e7
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-05-04 01:20:23 -0300

    Update project Item

--- web/src/components/ProjectItem/index.js
+++ web/src/components/ProjectItem/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-// import { Container } from './styles';
+import "./styles.css";
+import todayInHistory from "../../static/images/today_in_history_print1.PNG";
+
+const images = {
+  todayInHistory: todayInHistory,
+};
 
 function ProjectItem(props) {
+  const Banner = styled.div`
+    background-image: url(${images[props.background]});
+    width: 190px;
+    height: 190px;
+    filter: grayscale(100%);
+    transition: all 0.2s ease-out;
+    background-size: cover;
+    background-position: center;
+    box-shadow: 7px 7px, -7px -7px;
+
+    &:hover {
+      background-image: url(${images[props.background]});
+      filter: grayscale(0%);
+      width: 250px;
+      height: 250px;
+      box-shadow: 0px 0px, 0px 0px;
+      border: 2px outset white;
+    }
+  `;
+
   return (
     <div className="project-item">
-        <div className="banner">{props.image}ok</div>
-        <h1 className="title">{props.title}</h1>
+      <Banner />
+      <h3 className="project-item-title">{props.title}</h3>
     </div>
   );
 }
