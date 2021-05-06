17cae416ab06b55bcd5938cf0b0950a5ef152f63
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-05-06 00:46:51 -0300

    Update page header for mobile view
    adjust navbar and create dropdown menu for mobile view

--- web/src/components/PageHeader/index.js
+++ web/src/components/PageHeader/index.js
@@ -1,43 +1,93 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import { IoClose } from "react-icons/io5";
+import { HiMenu } from "react-icons/hi";
 
-import './styles.css';
+import "./styles.css";
 
 function PageHeader() {
-    const [scrolled, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const changeBackground = () => {
-        if(window.scrollY >= 200){
-            setScrolled(true);
-        } else {
-            setScrolled(false);
-        }
-    }
-    
-    useEffect(() => {
-        window.addEventListener('scroll', changeBackground);
-    })
-    
-    let x=['page-header'];
-    if(scrolled){
-        x.push('scrolled');
-    }
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    return (
-        <div className={x.join(" ")}>
-            <div className="logo">
-                <h3>Lucas OLIVEIRA</h3>
-            </div>
-            <div className="menu">
-                <HashLink smooth to="/#home" className="menu-item">Home</HashLink>
-                <HashLink smooth to="/#about" className="menu-item">About</HashLink>
-                <HashLink smooth to="/#services" className="menu-item">Services</HashLink>
-                <HashLink smooth to="/#projects" className="menu-item">Projects</HashLink>
-                <HashLink smooth to="/#contact" className="menu-item">Contacts</HashLink>
-            </div>
+  const changeBackground = () => {
+    if (window.scrollY >= 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+  });
+
+  let x = ["page-header"];
+  if (scrolled) {
+    x.push("scrolled");
+  }
+
+  return (
+    <div className={scrolled ? "page-header scrolled" : "page-header"}>
+      <div className="logo">
+        <h3>Lucas OLIVEIRA</h3>
+      </div>
+      <div className="top-menu">
+        <div className={click ? "menu active" : "menu"}>
+          <HashLink
+            smooth
+            to="/#home"
+            className="menu-item"
+            onClick={closeMobileMenu}
+          >
+            Home
+          </HashLink>
+          <HashLink
+            smooth
+            to="/#about"
+            className="menu-item"
+            onClick={closeMobileMenu}
+          >
+            About
+          </HashLink>
+          <HashLink
+            smooth
+            to="/#services"
+            className="menu-item"
+            onClick={closeMobileMenu}
+          >
+            Services
+          </HashLink>
+          <HashLink
+            smooth
+            to="/#projects"
+            className="menu-item"
+            onClick={closeMobileMenu}
+          >
+            Projects
+          </HashLink>
+          <HashLink
+            smooth
+            to="/#contact"
+            className="menu-item"
+            onClick={closeMobileMenu}
+          >
+            Contacts
+          </HashLink>
         </div>
-    );
+        <div className="mobile-menu" onClick={handleClick}>
+          {click ? (
+            <IoClose className="menu-icon" />
+          ) : (
+            <HiMenu className="menu-icon" />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PageHeader;
