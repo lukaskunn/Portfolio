e786f2b789ec31f66d16c7afd785fb23e97296fe
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-07-26 23:40:09 -0300

    change the project item style

--- web/src/components/ProjectItem.jsx
+++ web/src/components/ProjectItem.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 
 import "../styles/project-item.css";
 
@@ -11,24 +10,9 @@ const images = {
 };
 
 function ProjectItem(props) {
-  const Banner = styled.div`
-    background-image: url(${images[props.background]});
-    width: 190px;
-    height: 190px;
-    filter: grayscale(100%);
-    transition: all 0.2s ease-out;
-    background-size: cover;
-    background-position: center;
-    box-shadow: 7px 7px, -7px -7px;
-
-    &:hover {
-      background-image: url(${images[props.background]});
-    }
-  `;
-
   return (
     <div className="project-item">
-      <Banner />
+      <img src={images[props.background]} className="banner-image" alt=""/>
       <h3 className="project-item-title">{props.title}</h3>
     </div>
   );
