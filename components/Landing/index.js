cf0a61b9fa67984c123181fececdc3722e5e5b38
Lucas Oliveira <lucassioliveira098@gmail.com>
2022-04-15 14:20:18 -0300

    feat: added ground mirror in home page

--- components/Landing/index.js
+++ components/Landing/index.js
@@ -2,22 +2,34 @@ import React, { useRef, useState } from "react";
 import styles from "./Landing.module.scss";
 
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Edges } from "@react-three/drei";
+import { Edges, Reflector } from "@react-three/drei";
+import { KernelSize } from "postprocessing";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
 function Box(props) {
   const ref = useRef();
   useFrame((state, delta) => {
-    ref.current.rotation.x += 0.005;
-    ref.current.rotation.y += 0.005;
-    ref.current.rotation.z += 0.005;
+    ref.current.rotation.x += 0.002;
+    ref.current.rotation.y += 0.002;
+    ref.current.rotation.z += 0.002;
   });
 
   return (
-    <mesh {...props} ref={ref} scale={2}>
+    <mesh {...props} ref={ref} scale={2.3} position={[0, 0, -5]}>
       <boxGeometry args={[1, 1, 1]} threshold={20} />
-      <meshPhongMaterial color="#000000" opacity={0} transparent />
+      <meshPhongMaterial color="white" opacity={0.01} transparent />
       <Edges color={"white"} scale={1} lineHeight={5} />
     </mesh>
+  );
+}
+
+function Ground(props) {
+  return (
+    <Reflector resolution={2024} args={[8, 8]} {...props} scale={3}>
+      {(Material, props) => (
+        <Material color="white" metalness={1} normalScale={[0, 0]} {...props} />
+      )}
+    </Reflector>
   );
 }
 
@@ -31,8 +43,25 @@ function Landing() {
       <div id={styles.canvas_container}>
         <Canvas className={styles.canvas_home}>
           <ambientLight />
-          <pointLight position={[0, 0, 0]} />
+          <pointLight position={[0, 5, -10]} />
           <Box position={[0, 0, 0]} />
+          <Ground
+            mirror={1}
+            blur={[100, 100]}
+            mixBlur={0.7}
+            mixStrength={0.15}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            position-y={-2}
+            position-z={-0}
+          />
+          <EffectComposer multisampling={8}>
+            <Bloom
+              kernelSize={3}
+              luminanceThreshold={0}
+              luminanceSmoothing={5}
+              intensity={0.4}
+            />
+          </EffectComposer>
         </Canvas>
       </div>
     </section>
