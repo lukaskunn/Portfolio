f5e6928cfbd95214cf1ae85f4a8c9a765b5c2dee
Lucas Oliveira <lucassioliveira098@gmail.com>
2022-04-15 13:22:50 -0300

    added threejs cube to landing page

--- components/Landing/index.js
+++ components/Landing/index.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Landing.module.scss";
+
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Edges } from "@react-three/drei";
+
+function Box(props) {
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 0.005;
+    ref.current.rotation.y += 0.005;
+    ref.current.rotation.z += 0.005;
+  });
+
+  return (
+    <mesh {...props} ref={ref} scale={2}>
+      <boxGeometry args={[1, 1, 1]} threshold={20} />
+      <meshPhongMaterial color="#000000" opacity={0} transparent />
+      <Edges color={"white"} scale={1} lineHeight={5} />
+    </mesh>
+  );
+}
 
 function Landing() {
   return (
@@ -7,11 +27,14 @@ function Landing() {
       <div className={styles.home__title}>
         <h1 className={styles.title}>LUCAS OLIVEIRA</h1>
         <h3 className={styles.subtitle}>Web Developer from São Paulo</h3>
-        <div className={styles.white_border}></div>
-        <div className={styles.white_border2}></div>
-        <div className={styles.white_border3}></div>
       </div>
-      <img src="https://i.imgur.com/UswyfSK.png" alt="" />
+      <div id={styles.canvas_container}>
+        <Canvas className={styles.canvas_home}>
+          <ambientLight />
+          <pointLight position={[0, 0, 0]} />
+          <Box position={[0, 0, 0]} />
+        </Canvas>
+      </div>
     </section>
   );
 }
