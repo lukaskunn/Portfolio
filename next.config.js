e63a5ff6dd917164c876654d97e6311e84e71ee0
Lucas Oliveira <lucassioliveira098@gmail.com>
2022-04-16 16:35:43 -0300

    update next configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["64.media.tumblr.com"],
+  },
 };
 
 const withTM = require("next-transpile-modules")(["three"]);
