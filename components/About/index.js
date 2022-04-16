548c02cbbbbf4a943669c1cd4c7da24c3f77f73d
Lucas Oliveira <lucassioliveira098@gmail.com>
2022-04-16 16:36:52 -0300

    feat: created about section

--- components/About/index.js
+++ components/About/index.js
@@ -1,10 +1,68 @@
 import React from "react";
 import about from "./About.module.scss";
+import { Controller, Scene } from "react-scrollmagic";
+import Image from "next/image";
 
 function About() {
   return (
-    <section className={about.about}>
-      <h1>Lucas Oliveira</h1>
+    <section className={about.about} id={"about"}>
+      <Controller>
+        <Scene
+          duration={1600}
+          pin
+          triggerElement={"#about"}
+          triggerHook={"onLeave"}
+        >
+          <div className={about.about__left}>
+            <img
+              src="https://64.media.tumblr.com/723de3a38fae9be2c3840107091e6f3d/tumblr_pg37vjbDgu1v1hotuo2_500.gifv"
+              alt=""
+              className="anime_computer_gif"
+            />
+          </div>
+        </Scene>
+      </Controller>
+      <div className={about.about__right}>
+        <h2>
+          Hello im Lucas Oliveira, Web Developer and technology enthusiast
+        </h2>
+        <section className={about.about__right__aboutMe}>
+          <h2>About Me</h2>
+          <h4>
+            I`m a 21-year old guy, a computer science student, and working as a
+            front-end developer for e-commerce with Vtex at
+            <a
+              href="https://www.corebiz.ag/en/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Corebiz.
+            </a>
+            <br /> <br /> Actually working on my Final Paper about Computer
+            Vision and leaf disease for college and developing for Motorola
+            India on my job.
+            <br />
+            <br /> I`ve been worked for a lot of e-commerce creating theirs home
+            pages and product pages, creating new components and fixing existing
+            components
+            <br />
+            Actually studying React frameworks to improve my knowledge about
+            front-end. I`m
+          </h4>
+        </section>
+        <section className={about.about__right__background}>
+          <h2>Background</h2>
+          <h4>
+            Competitive programming. <br />
+            Go out with friends. <br />
+            Animes. <br />
+            Rock song. <br />
+            watch movies. <br />
+            Play Elden Ring and League of Legends.
+          </h4>
+          <br />
+        </section>
+      </div>
     </section>
   );
 }
