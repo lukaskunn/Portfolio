3f2758ef3add55ff2f7bc0d11867c21b5e6f0b75
Lucas Oliveira <lucassioliveira098@gmail.com>
2022-06-20 21:35:24 -0300

    feat: prevent translate in chrome

--- next.config.js
+++ next.config.js
@@ -10,3 +10,9 @@ const withTM = require("next-transpile-modules")(["three"]);
 
 module.exports = withTM();
 module.exports = nextConfig;
+module.exports = {
+  i18n: {
+    locales: ["pt"],
+    defaultLocale: "pt",
+  },
+};
