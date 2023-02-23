0653b6a84020fc41af5da13f9b003d7fd3dfb794
lucas silva oliveira <midasmatrix123@gmail.com>
2023-02-22 23:31:12 -0300

    redesign of home landing page

---
+++ components/Landing/components/CustomBackgroundImages/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import customBackground from "./CustomBackgroundImages.module.scss";
+
+function CustomBackgroundImages() {
+  return <div />;
+}
+
+export default CustomBackgroundImages;
