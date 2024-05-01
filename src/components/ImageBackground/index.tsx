60e7e34fadff3d0d16522efbd14e015a5649476c
lukaskunn <lucassioliveira098@gmail.com>
2024-05-01 19:39:01 -0300

    feat: updated background images url

--- src/components/ImageBackground/index.tsx
+++ src/components/ImageBackground/index.tsx
@@ -7,7 +7,15 @@ function ImageBackground() {
   //TODO: Fix any type
   const {setIsLoaded} = React.useContext(PageContext) as any;
   const [imagesLoaded, setImagesLoaded] = React.useState(0);
-  const [images, setImages] = React.useState([]);
+  const images = [
+    "https://pgbsmbfirsngiasjvnvy.supabase.co/storage/v1/object/public/landing_page/20210803_031452-scaled.jpg",
+      "https://pgbsmbfirsngiasjvnvy.supabase.co/storage/v1/object/public/landing_page/27159639._SX540_.jpg",
+      "https://pgbsmbfirsngiasjvnvy.supabase.co/storage/v1/object/public/landing_page/4c7ff6256079957c2770ea922741815e.jpg",
+      "https://pgbsmbfirsngiasjvnvy.supabase.co/storage/v1/object/public/landing_page/7108634.png",
+      "https://pgbsmbfirsngiasjvnvy.supabase.co/storage/v1/object/public/landing_page/Ghost-in-the-shell_ButWhyTho.png",
+      "https://pgbsmbfirsngiasjvnvy.supabase.co/storage/v1/object/public/landing_page/alita-.png",
+      "https://pgbsmbfirsngiasjvnvy.supabase.co/storage/v1/object/public/landing_page/images.jpg"
+  ]
   const mousePosition = useMousePosition();
   const [dimensions, setDimensions] = React.useState({
     width: 0,
@@ -33,18 +41,6 @@ function ImageBackground() {
     }
   }, [imagesLoaded, images, setIsLoaded])
 
-React.useEffect(() => {
-    axios.get("/api/getImages")
-    .then((response) => {
-      const {blobs} = response.data
-      const images = blobs.filter((image: any) => image.size > 0)
-      setImages(images)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}, [setIsLoaded])
-
 const backgroundImageStyles = {
   left: `${-((mousePosition.x / dimensions.width) - 0.5) * 30}px`,
   top: `${(-((mousePosition.y / dimensions.height) - 0.5) * 30) + 100}px`,
@@ -57,7 +53,7 @@ const backgroundImageStyles = {
         return (
           <img
             alt={`image_${index + 1}`}
-            src={image.url}
+            src={image}
             className={`${styles[`image_${index + 1}`]} ${styles["single-image"]}`}
             key={index}
             onLoad={() => setImagesLoaded(imagesLoaded + 1)}
