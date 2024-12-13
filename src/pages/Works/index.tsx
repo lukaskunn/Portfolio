e4bfd7f018f46387cf9d1c76a8709530aac31372
Lucas Oliveira <lucassioliveira098@gmail.com>
2024-12-12 22:23:40 -0300

    feat: add my stuff to my projects

--- src/pages/Works/index.tsx
+++ src/pages/Works/index.tsx
@@ -1,7 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import ProjectCard from "../../components/ProjectCard";
 import { PageContext } from "../../contexts/PageContext";
 import styles from "../../styles/Works.module.scss";
 import Curve from "../../Layouts/Curve";
@@ -11,9 +10,37 @@ import ProjectModal from "../../components/ProjectModal";
 
 function Works() {
   const { language } = React.useContext(PageContext) as any;
-  const {works} = language;
-  const {sectionTitle, nextPageText, seeMoreText, personalProjects, backgroundProjects} = works;
-  const [modal, setModal] = useState({isActive: false, index: 0});
+  const { works } = language;
+  const { sectionTitle, nextPageText, personalProjects, backgroundProjects } =
+    works;
+  const [modal, setModal] = useState({
+    isActive: false,
+    index: 0,
+    projects: backgroundProjects.projects,
+  });
+
+  const changeModalList = (index: number) => {
+    console.log(index)
+    const modalList =
+      index === 0 ? personalProjects.projects : backgroundProjects.projects;
+    console.log(modalList);
+    console.log("resetting modal");
+
+    setModal({ isActive: false, index: 0, projects: modalList });
+  };
+
+  const updateModal = (index: number, modalIsActive: boolean) => {
+    setModal({ isActive: modalIsActive, index, projects: modal.projects });
+  };
+
+  React.useEffect(() => {
+    setTimeout(() => {
+        console.log("1");
+        setModal({ isActive: false, index: 0, projects: backgroundProjects.projects });
+    }, 100)
+    console.log(2)
+}, []);
+
 
   return (
     <Curve>
@@ -25,34 +52,34 @@ function Works() {
             id={styles.controlledTabs}
             defaultIndex={1}
             focusTabOnClick={false}
+            onSelect={(index) => changeModalList(index)}
           >
             <TabList>
               <Tab>{personalProjects.title}</Tab>
               <Tab>{backgroundProjects.title}</Tab>
             </TabList>
             <TabPanel>
-              {personalProjects.projects &&
-              personalProjects.projects.length == 0 ? (
-                <h2>nothing to show yet!</h2>
-              ) : (
-                personalProjects.projects.map(
-                  (project: any, index: any) => {
-                    const { projectTitle, background, description, urlToProject } = project;
+              <div className={styles.projectGrid}>
+                {personalProjects.projects &&
+                personalProjects.projects.length < 1 ? (
+                  <h2>nothing to show yet!</h2>
+                ) : (
+                  personalProjects.projects.map((project: any, index: any) => {
                     return (
-                      <>
-                        <ProjectCard
-                          key={index}
-                          title={projectTitle}
-                          background={background}
-                          description={description}
-                          urlToProject={urlToProject}
-                          texToToProject={seeMoreText}
-                        />
-                      </>
+                      <ProjectItem
+                        projectId={project.projectId}
+                        key={index}
+                        index={index}
+                        title={project.title}
+                        updateModal={updateModal}
+                        description={project.description}
+                        goToExternalPage={project.goToExternalPage}
+                        urlToProject={project.urlToProject}
+                      />
                     );
-                  }
-                )
-              )}
+                  })
+                )}
+              </div>
             </TabPanel>
             <TabPanel>
               <div className={styles.projectGrid}>
@@ -60,27 +87,36 @@ function Works() {
                 backgroundProjects.projects.length < 1 ? (
                   <h2>nothing to show yet!</h2>
                 ) : (
-                  backgroundProjects.projects.map((project: any, index: any) => {
-                    return (
-                      <ProjectItem
-                        projectId={project.projectId}
-                        key={index}
-                        index={index}
-                        title={project.title}
-                        setModal={setModal}
-                        description={project.description}
+                  backgroundProjects.projects.map(
+                    (project: any, index: any) => {
+                      return (
+                        <ProjectItem
+                          projectId={project.projectId}
+                          key={index}
+                          index={index}
+                          title={project.title}
+                          updateModal={updateModal}
+                          description={project.description}
                         />
-                    )})
+                      );
+                    }
+                  )
                 )}
               </div>
-              <ProjectModal modal={modal} projects={backgroundProjects.projects}/>
             </TabPanel>
           </Tabs>
+          <ProjectModal modal={modal} />
         </div>
       </section>
-      <NextPageButton link="/About" text={nextPageText} type="forward" showBackground={true}/>
+      <NextPageButton
+        link="/About"
+        text={nextPageText}
+        type="forward"
+        showBackground={true}
+      />
     </Curve>
   );
 }
 
 export default Works;
+
